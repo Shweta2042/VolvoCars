7c1ebc4e5257ef8cefad989af5cb55209116a31c
Shwetha Kumari <shweta.kumari@yellow.ai>
2023-02-22 19:07:30 +0530

    Media-quries-fix

--- components/NextArrow.tsx
+++ components/NextArrow.tsx
@@ -1,10 +1,17 @@
 import Image from 'next/image'
+import { useEffect, useRef } from 'react';
 import arrow from '../docs/chevron-circled.svg'
 
 export default function SampleNextArrow(props) {
     const { className, onClick } = props;
+    const ref= useRef(null);
+    useEffect(
+      function(){
+        ref?.current?.focus();
+      },[]
+    )
     return (
-      <button
+      <button aria-label="Next arrow" ref={ref}
         className={className}
         onClick={onClick}>
           <Image  src={arrow} alt='next'  width={60} height={60} />
